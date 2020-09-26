cd93480de237c6fc1ccaf4155b84c1045486ea87
Monica <vieiramonica71@gmail.com>
2020-09-26 23:12:13 +0100

    website (sobre,capa)

---
+++ Portefolio/Js/main2.js
@@ -0,0 +1,24 @@
+
+//MENU DROPDOWN W3
+function myFunction() { 
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) { 
+      x.className += " w3-show";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+
+  //NAVEBAR
+  function w3_open() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+
+
+
